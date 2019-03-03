abfe4c34b9d08f36ab779d908a28c71a864cfa09
Uma Subbiah <auriflore1@gmail.com>
2019-03-03 08:55:50 +0530

    Database connectivity for caterer fixed

--- Employee Portal/index.js
+++ Employee Portal/index.js
@@ -1,7 +1,11 @@
+var alert = require('alert-node');
+const express = require('express');
+const app = express();
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var sql=require('mssql');
+var bodyParser = require('body-parser');
 var formidable=require('formidable');
 var config = {
 	user:'abhilash.venky',
@@ -15,86 +19,20 @@ var config = {
 http.createServer(function (req, res) 
 {
 	var q = url.parse(req.url, true);
-	var filename = "./pages/static/enter_caterer.html" ;
-
+	var filename = "./pages/static" + q.pathname;
+	if(filename[filename.length-1]=='/')
+		filename+='enter_caterer.html';
 	console.log(q.pathname);
-	if(q.pathname=='/ContactUs')
+	
+	if(q.pathname=='/submit')
 	{
-		var x=0;
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		console.log("ContactUs\n")
-         {
-			try {
-				// connect to your database
-				console.log("Hi3")
-				sql.connect(config, function (err) {
-				
-					if (err){ console.log(err);console.log("ERROR");}
-			
-					// create Request object
-					var request = new sql.Request();
-					console.log("Hi2")
-					// query to the database and get the records
-					request.query('select * from contact', function (err, recordset) {
-						console.log("Hi1")
-						if (err) console.log(err)
-			
-						// send records as a response
-						console.log(recordset);
-						var pr='';
-						
-						for(i in recordset['recordset'])
-						{
-							console.log(recordset['recordset'][i]);
-							pr+='<div class="col-sm-4">';
-								pr+='<table class="table">';
-								pr+='<tr><th>'+recordset['recordset'][i]['position']+'</th></tr>';
-								pr+='<tr><td>'+recordset['recordset'][i]['name']+'</td></tr>';
-								pr+='<tr><td>'+recordset['recordset'][i]['email']+'</td></tr>';
-								pr+='<tr><td>'+recordset['recordset'][i]['phone']+'</td></tr>';
-								pr+='<tr><td>'+recordset['recordset'][i]['phone2']+'</td></tr>';
-								pr+='</table>';
-							pr+='</div>';
-						}
-						//console.log(pr);
-						fs.readFile('./pages/static/ContactUs.html', function(err, data){
-							var st=data.toString();
-							console.log(data.toString());
-							st=st.replace('~1',pr);
-							res.write(st);
-							res.end();
-						});
-						sql.close();
-					});
-				});
-				
-			} catch (err) {
-				console.log(err);console.log("ERROR");
-			}
-			console.log("End");
-		}
-		console.log("End");
-		return;
-	}
-	else if(q.pathname=='/enter_caterer')
-	{
-		//filename+='.html';
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		fs.readFile('./pages/static/enter_caterer.html', function(err, data){
-			var st=data.toString();
-			console.log(data.toString());
-			res.write(st);
-			res.end();
-			return;
-		});
-	}
-	else if(q.pathname=='/register/submit')
-	{
+		console.log("Hi");
 		var form = new formidable.IncomingForm();
 		form.parse(req,function(err,fields,files) 
 		{
 			sql.connect(config, function (err){
 				var request = new sql.Request();
+				
 				request.input('cname',fields['cname']);
 				request.input('lname',fields['lname']);
 				request.input('mobile',fields['mobile']);
@@ -103,15 +41,18 @@ http.createServer(function (req, res)
 				request.input('phone',fields['phone']);
 				request.input('service',fields['service']);
 				request.input('city',fields['city']);
-				request.query("insert into test_caterer(cname,lname,mobile,email,oaddress,phone,service,city) values(@cname,@lname,@mobile,@email,@oaddress,@phone,@service,@city);",function(err, result){
+				console.log('${req.body.cname}');
+				request.query("insert into test_caterer2(name,contact_person,mobile,email,oaddress,phone,service,city) values(@cname,@lname,@mobile,@email,@oaddress,@phone,@service,@city);",function(err, result){
 					console.log(result);
-					console.log(fields['cname']+err);
+					console.log(fields['cname']+"\n"+err);
 					sql.close();
-					res.end();
-					return;
+					
+
+					alert('Caterer details entered successfully')
 				});
 			});
 		});
+		
 	}
 	else	
 	{
@@ -131,4 +72,8 @@ http.createServer(function (req, res)
     		return res.end();
   		});
 	}
-}).listen(3030);
+	res.writeHead(301,
+		{ Location: 'http://localhost:3000/employee_landing_page.html'  }
+	  );
+	  res.end();
+}).listen(8088);
