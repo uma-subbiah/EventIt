568a93df03df3ad1d2359bd965f59b5cc8828d86
Akshaya Sundaram <32802109+akshaya-sundaram@users.noreply.github.com>
2019-03-04 21:08:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,86 +23,79 @@ function parseCookies(request) {
     });
     return list;
 }
-  http.createServer(function (req, res) 
-{
-	console.log("LOG: req url :"+req.url);
-	var q = url.parse(req.url, true);
-	var filename = "./pages/static" + q.pathname;
-	if(filename[filename.length-1]=='/')
-	{
-		filename+='index.html';
-		console.log("LOG: Landing page requested by a client... ");
-	}
-	if(q.pathname=='/ContactUs')
-	{
-		var x=0;
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		console.log("\nLOG: ContactUs page requested by a client...")
-			console.log("LOG: Attempting DB Connection...")
-			try {
-				// connect to your database
-				sql.connect(config, function (err) {
-				
-					if (err){console.log("!!!LOG: Error in connection to database. "); res.write("There's a DB server connection error. That's all we can say right now. :/"); return;}
-			
-					// create Request object
-					var request = new sql.Request();
-					console.log("LOG: Request object created ...")
-					// query to the database and get the records
-					request.query('select * from contact', function (err, recordset) {
-						console.log("LOG: Running SQL Query using the request object ...")
-						if (err) console.log("!!!LOG: Error returned by database.")
-			
-						// send records as a response
-						console.log("LOG: Query success... : ");
-						var pr='';
-						
-						for(i in recordset['recordset'])
-						{
-							console.log(recordset['recordset'][i]);
-							pr+='<div class="col-sm-4">';
-								pr+='<table class="table">';
-								pr+='<tr><th>'+recordset['recordset'][i]['position']+'</th></tr>';
-								pr+='<tr><td>'+recordset['recordset'][i]['name']+'</td></tr>';
-								pr+='<tr><td>'+recordset['recordset'][i]['email']+'</td></tr>';
-								pr+='<tr><td>'+recordset['recordset'][i]['phone']+'</td></tr>';
-								pr+='<tr><td>'+recordset['recordset'][i]['phone2']+'</td></tr>';
-								pr+='</table>';
-							pr+='</div>';
-						}
-						//console.log(pr);
-						fs.readFile('./pages/static/ContactUs.html', function(err, data){
-							var st=data.toString();
-							console.log(data.toString());
-							st=st.replace('~1',pr);
-							res.write(st);
-							res.end();
-						});
-						sql.close();
-					});
-				});
-				
-			} catch (err) {
-				console.log("!!!LOG : Error in fetching ContactUs page... : ");
-				console.log(err);
-			}	
-		console.log("LOG: Ending ContactUs request block ... ");
-		return;
-	}
-    
-	else if(q.pathname=='/register')
-	{
-		//filename+='.html';
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		fs.readFile('./pages/static/registration.html', function(err, data){
-			var st=data.toString();
-			console.log(data.toString());
-			res.write(st);
-			res.end();
-			return;
-		});
-	}
-  else if (q.pathname == '/register/submit') {
+http.createServer(function(req, res) {
+    console.log("LOG: req url :" + req.url);
+    var q = url.parse(req.url, true);
+    var filename = "./pages/static" + q.pathname;
+    if (filename[filename.length - 1] == '/') {
+        filename += 'index.html';
+        console.log("LOG: Landing page requested by a client... ");
+    }
+    if (q.pathname == '/ContactUs') {
+        var x = 0;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        console.log("\nLOG: ContactUs page requested by a client...")
+        console.log("LOG: Attempting DB Connection...")
+        try {
+            // connect to your database
+            sql.connect(config, function(err) {
+
+                if (err) { console.log("!!!LOG: Error in connection to database. ");
+                    res.write("There's a DB server connection error. That's all we can say right now. :/"); return; }
+
+                // create Request object
+                var request = new sql.Request();
+                console.log("LOG: Request object created ...")
+                    // query to the database and get the records
+                request.query('select * from contact', function(err, recordset) {
+                    console.log("LOG: Running SQL Query using the request object ...")
+                    if (err) console.log("!!!LOG: Error returned by database.")
+
+                    // send records as a response
+                    console.log("LOG: Query success... : ");
+                    var pr = '';
+
+                    for (i in recordset['recordset']) {
+                        console.log(recordset['recordset'][i]);
+                        pr += '<div class="col-sm-4">';
+                        pr += '<table class="table">';
+                        pr += '<tr><th>' + recordset['recordset'][i]['position'] + '</th></tr>';
+                        pr += '<tr><td>' + recordset['recordset'][i]['name'] + '</td></tr>';
+                        pr += '<tr><td>' + recordset['recordset'][i]['email'] + '</td></tr>';
+                        pr += '<tr><td>' + recordset['recordset'][i]['phone'] + '</td></tr>';
+                        pr += '<tr><td>' + recordset['recordset'][i]['phone2'] + '</td></tr>';
+                        pr += '</table>';
+                        pr += '</div>';
+                    }
+                    //console.log(pr);
+                    fs.readFile('./pages/static/ContactUs.html', function(err, data) {
+                        var st = data.toString();
+                        console.log(data.toString());
+                        st = st.replace('~1', pr);
+                        res.write(st);
+                        res.end();
+                    });
+                    sql.close();
+                });
+            });
+
+        } catch (err) {
+            console.log("!!!LOG : Error in fetching ContactUs page... : ");
+            console.log(err);
+        }
+        console.log("LOG: Ending ContactUs request block ... ");
+        return;
+    } else if (q.pathname == '/register') {
+        //filename+='.html';
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        fs.readFile('./pages/static/registration.html', function(err, data) {
+            var st = data.toString();
+            console.log(data.toString());
+            res.write(st);
+            res.end();
+            return;
+        });
+    } else if (q.pathname == '/register/submit') {
         var form = new formidable.IncomingForm();
         form.parse(req, function(err, fields, files) {
             sql.connect(config, function(err) {
@@ -128,22 +121,34 @@ function parseCookies(request) {
                 });
             });
         });
-	}
-	else if(q.pathname == '/login/submit')
-	{
-		
-	}
-    else if (q.pathname == '/login') 
-    {
+    } else if (q.pathname == '/login/submit') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function(err, fields, files) {
+            sql.connect(config, function(err) {
+                var request = new sql.Request();
+                request.input('mail', fields['email']);
+                request.input('password', fields['password']);
+
+                request.query("select password from customer where email=mail;", function(err, result) {
+
+                    sql.close();
+                    if (result.output == password) {
+                        res.write('<head><meta http-equiv="refresh" content="0; URL=http://www.google.com" /></head>');
+                        res.end();
+                        return;
+                    }
+
+                });
+            });
+        });
+    } else if (q.pathname == '/login') {
         var cookies = parseCookies(req);
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        if (false && (cookies['LoggedInCustID'] != null && cookies['LoggedInCustID'] != "-1")) 
-        {
-            
+        if (false && (cookies['LoggedInCustID'] != null && cookies['LoggedInCustID'] != "-1")) {
+
             res.write('<head><meta http-equiv="refresh" content="0; URL=http://www.eventit.com/landing/" /></head>');
             res.end();
-        }
-        else{
+        } else {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             fs.readFile('./pages/static/login.html', function(err, data) {
                 res.write(data.toString());
@@ -151,28 +156,23 @@ function parseCookies(request) {
                 return;
             });
         }
+    } else {
+        fs.readFile(filename, function(err, data) {
+            if (err) {
+                console.log(filename + "NOT FOUND!\n")
+                fs.readFile('./pages/static/404/index.html', function(err, data1) {
+                    var st = data1.toString();
+                    res.write(st);
+                    console.log(st)
+                    res.end();
+                });
+                return;
+            }
+            if (filename[filename.length - 1] == '/') {
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+            }
+            res.write(data);
+            return res.end();
+        });
     }
-	    else	
-	    {
-		fs.readFile(filename, function(err, data) 
-		{
-			if (err) 
-			{
-				console.log(filename+"NOT FOUND!\n")
-				fs.readFile('./pages/static/404/index.html',function (err, data1){
-					var st=data1.toString();
-					res.write(st);
-					console.log(st)
-					res.end();
-				});
-				return;
-			}	 
-			if(filename[filename.length-1]=='/') 
-    		{
-				res.writeHead(200, {'Content-Type': 'text/html'});
-			}
-    		res.write(data);
-    		return res.end();
-  		});
-	}
 }).listen(80);
