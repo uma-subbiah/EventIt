7182401ef321cf9d95d97dd42e8202ef5914cf21
abhilashvenky <40466264+abhilashvenky@users.noreply.github.com>
2019-03-20 23:13:05 +0530

    Ugh

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var url = require('url');
 var fs = require('fs');
 var sql = require('mssql');
 var formidable = require('formidable');
-var utils=require('./utilities.js')
 var awsservices = require('./AWSnodeproj/sns_publishsms')
+
 var config = {
     user: 'abhilash.venky',
     password: 'Ninju123',
@@ -25,61 +25,17 @@ function parseCookies(request) {
     });
     return list;
 }
-  http.createServer(function (req, res)
+  http.createServer(function (req, res) 
 {
-	//console.log("LOG: req url :"+req.url);
+	console.log("LOG: req url :"+req.url);
 	var q = url.parse(req.url, true);
 	var filename = "./pages/static" + q.pathname;
 	if(filename[filename.length-1]=='/')
 	{
 		filename+='index.html';
 		console.log("LOG: Landing page requested by a client... ");
-    }
-    if(q.pathname=='/AdminLanding')
-    {
-        try {
-            // connect to your database
-            sql.connect(config, function (err) {
-                if (err){console.log("!!!LOG: Error in connection to database. "); res.write("There's a DB server connection error. That's all we can say right now. :/"); return;}
-                // create Request object
-                var request = new sql.Request();
-                console.log("LOG: Request object created ...")
-                // query to the database and get the records
-                request.query('select * from rev_sum', function (err, recordset) {
-                    console.log("LOG: Running SQL Query using the request object ...")
-                    if (err)
-                    {
-                        console.log("!!!LOG: Error returned by database.")
-                        return res.end();
-                    }
-                    // send records as a response
-                    console.log("LOG: Query success... : ");
-                    var pr='';
-                    var y=recordset['recordset'];
-                    var z=[[y[4]['1'],y[3]['1'],y[2]['1'],y[1]['1'],y[0]['1']],[y[4]['2'],y[3]['2'],y[2]['2'],y[1]['2'],y[0]['2']],[y[4]['3'],y[3]['3'],y[2]['3'],y[1]['3'],y[0]['3']],[y[4]['4'],y[3]['4'],y[2]['4'],y[1]['4'],y[0]['4']],[y[4]['5'],y[3]['5'],y[2]['5'],y[1]['5'],y[0]['5']]];
-                    pr=require('JSON').stringify(z);
-                    console.log("LOG : "+pr);
-                    fs.readFile('./pages/static/ChartFeedback/index.html', function(err, data){
-                        var st=data.toString();
-                       // console.log(data.toString());
-                        st=st.replace('~1',pr);
-                        console.log(st);
-                        res.write(st);
-                        res.end();
-                        sql.close();
-                        return;
-
-                    });
-
-                });
-            });
-
-        } catch (err) {
-            console.log("!!!LOG : Error in fetching ContactUs page... : ");
-            console.log(err);
-        }
-    }
-	else if(q.pathname=='/ContactUs')
+	}
+	if(q.pathname=='/ContactUs')
 	{
 		var x=0;
 		res.writeHead(200, {'Content-Type': 'text/html'});
@@ -88,9 +44,9 @@ function parseCookies(request) {
 			try {
 				// connect to your database
 				sql.connect(config, function (err) {
-
+				
 					if (err){console.log("!!!LOG: Error in connection to database. "); res.write("There's a DB server connection error. That's all we can say right now. :/"); return;}
-
+			
 					// create Request object
 					var request = new sql.Request();
 					console.log("LOG: Request object created ...")
@@ -98,11 +54,11 @@ function parseCookies(request) {
 					request.query('select * from contact', function (err, recordset) {
 						console.log("LOG: Running SQL Query using the request object ...")
 						if (err) console.log("!!!LOG: Error returned by database.")
-
+			
 						// send records as a response
 						console.log("LOG: Query success... : ");
 						var pr='';
-
+						
 						for(i in recordset['recordset'])
 						{
 							console.log(recordset['recordset'][i]);
@@ -127,15 +83,15 @@ function parseCookies(request) {
 						sql.close();
 					});
 				});
-
+				
 			} catch (err) {
 				console.log("!!!LOG : Error in fetching ContactUs page... : ");
 				console.log(err);
-			}
+			}	
 		console.log("LOG: Ending ContactUs request block ... ");
 		return;
 	}
-
+    
 	else if(q.pathname=='/register')
 	{
 		//filename+='.html';
@@ -152,28 +108,21 @@ function parseCookies(request) {
         var form = new formidable.IncomingForm();
         form.parse(req, function(err, fields, files) {
             sql.connect(config, function(err) {
-                var request = new sql.Request();
-				request.input('eventID', parseInt(fields['eventID']));
+				var request = new sql.Request();
+				request.input('eventID', fields['eventID']);
                 request.input('name', fields['name']);
                 request.input('email', fields['email']);
-                request.input('overallexp', parseInt(fields['overallexp']));
-                request.input('managerbehaviour', parseInt(fields['managerbehaviour']));
-                request.input('foodrating', parseInt(fields['foodrating']));
-                request.input('entertainmentrating', parseInt(fields['entertainmentrating']));
-				request.input('futurebookingpreference', parseInt(fields['futurebookingpreference']));
+                request.input('overallexp', fields['overallexp']);
+                request.input('managerbehaviour', fields['managerbehaviour']);
+                request.input('foodrating', fields['foodrating']);
+                request.input('entertainmentrating', fields['entertainmentrating']);
+				request.input('futurebookingpreference', fields['futurebookingpreference']);
 				request.input('additionalcomments', fields['message']);
                 request.query("insert into review(eventID,overallExp,managerbehaviour,foodrating,entertainmentrating,futurebookingpreference,additionalComments) values(@eventID,@overallexp,@managerbehaviour,@foodrating,@entertainmentrating,@futurebookingpreference,@additionalComments);", function(err, result) {
-                    console.log('LOG : futurebookingpref .. :'+fields['futurebookingpreference']);
-                    if(err)
-                    {
-                        console.log(err.toString());
-                        res.end();
-                        return;
-                    }
-                    console.log("LOG: query result"+result);
+                    console.log(result);
                     //console.log(fields['fname'] + " " + fields['lname'] + "\n" + err);
                     sql.close();
-                    res.write('<head><meta http-equiv="refresh" content="0; URL=http://localhost:8080/reviewsuccess" /></head>');
+                    res.write('Check console logs for review status.');
                     res.end();
                     return;
                 });
@@ -184,7 +133,6 @@ function parseCookies(request) {
         var form = new formidable.IncomingForm();
         form.parse(req, function(err, fields, files) {
             sql.connect(config, function(err) {
-                if (err) throw err;
                 var request = new sql.Request();
                 request.input('fname', fields['fname']);
                 request.input('lname', fields['lname']);
@@ -202,45 +150,17 @@ function parseCookies(request) {
 					console.log("LOG: REG SUCCESSFUL!");
                     //console.log(fields['fname'] + " " + fields['lname'] + "\n" + err);
                     sql.close();
-
-
-                    res.write('<head><meta http-equiv="refresh" content="0; URL=http://localhost:8080/regsuccess" /></head>');
-
+                    res.write('<head><meta http-equiv="refresh" content="0; URL=http://localhost/regsuccess" /></head>');
                     res.end();
 				});
 				var sendText = "Dear " + fields['fname'] + ", you have been successfully registered on EventIt.com! Please feel free to log in anytime at http://eventit.com/login";
-                awsservices.sendSMS(sendText, fields['mobile']);
-                utils.MailSend(fields['email'],sendText);
+				awsservices.sendSMS(sendText, fields['mobile']);
             });
         });
 	}
-  else if (q.pathname == '/login/submit') {
-        var form = new formidable.IncomingForm();
-        form.parse(req, function(err, fields, files) {
-            sql.connect(config, function(err) {
-                var request = new sql.Request();
-                request.input('mail', fields['email']);
-                console.log("LOG: email : " + fields['email']);
-                request.query("select password from customer where email=@mail;", function(err, result) {
-                    //console.log("LOG : Login query result : "+result);
-                    //console.log("LOG : Login query result : "+result["recordsets"][0][0].password);
-                    if (err) {
-                        console.log("!!!LOG: Error in query retrieval... : " + err);
-                        return;
-                    }
-                    sql.close();
-                    if (result["recordset"][0]["password"] == fields['password']) {
-                        res.write('<head><meta http-equiv="refresh" content="0; URL=/customer_ui/clanding.html" /></head>');
-                        res.end();
-                        return;
-                    }
-                });
-            });
-        });
-    }
-	else if (q.pathname == '/regsuccess')
+	else if (q.pathname == '/regsuccess') 
     {
-        fs.readFile('./pages/static/RegSuccess/index.html', function(err, data)
+        fs.readFile('./pages/static/RegSuccess/index.html', function(err, data) 
 		{
 			if(err) throw err;
 			var st=data.toString();
@@ -248,29 +168,17 @@ function parseCookies(request) {
 			res.end();
 		});
     }
-    else if (q.pathname == '/reviewsuccess')
-    {
-        console.log('LOG: review/success detected');
-        fs.readFile('./pages/static/RegSuccess/index.html', function(err, data)
-		{
-			if(err) throw err;
-            var st=data.toString();
-            st=st.replace('Successfully Registered!','Thanks for submitting the review!');
-            st=st.replace('Your account has been registered! Check your phone for confirmation.','Your review has been recorded. Thanks.We\'ll improve from it (hopefully, that is).')
-            st=st.replace('Registration Success!','Review Submitted!')
-            res.write(st);
-			res.end();
-		});
-    }
-    else if (q.pathname == '/login')
+    else if (q.pathname == '/login') 
     {
         var cookies = parseCookies(req);
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        if (false && (cookies['LoggedInCustID'] != null && cookies['LoggedInCustID'] != "-1")) {
-
+        if (false && (cookies['LoggedInCustID'] != null && cookies['LoggedInCustID'] != "-1")) 
+        {
+            
             res.write('<head><meta http-equiv="refresh" content="0; URL=http://www.eventit.com/landing/" /></head>');
             res.end();
-        } else {
+        }
+        else{
             res.writeHead(200, { 'Content-Type': 'text/html' });
             fs.readFile('./pages/static/login.html', function(err, data) {
                 res.write(data.toString());
@@ -278,52 +186,12 @@ function parseCookies(request) {
                 return;
             });
         }
-    } else if (q.pathname == '/customer_ui/askevent/submit') {
-        console.log("LOG: Event ask detected ...");
-        var form = new formidable.IncomingForm();
-        form.parse(req, function(err, fields, files) {
-            if (err) {
-                console.log("!!!LOG: Error in parsing form... : " + err.toString());
-                return;
-            }
-            sql.connect(config, function(err) {
-
-                if (err) {
-                    console.log("!!!LOG: Error in connection... : " + err.toString());
-                    return;
-                }
-                var request = new sql.Request();
-                request.input('mobile', fields['mobile']);
-                request.input('email', fields['email']);
-                request.input('budget', fields['budget']);
-                request.input('category', fields['event']);
-                request.input('location', fields['location']);
-                request.query("insert into event(Category,eventLocation,budget,email) values(@category,@location,@budget,@email)", function(err, result) {
-                    if (err) {
-                        console.log("!!!LOG: Error in query retrieval... : " + err.toString());
-                        //return;
-                    }
-                    console.log(result);
-                    console.log(fields['budget'] + " " + fields['event']+ " " + fields['email']+ " " + fields['location'] + "\n" + err);
-                    sql.close();
-                    res.end();
-                    var sendText = "Greetings from EventIt! Your event is under process! Please feel free to track your event at http://eventit.com/login";
-                    try{
-                        awsservices.sendSMS(sendText, fields['mobile']);
-                    } catch(err){console.log("AWS error.close.close.Abhilash's fault")};
-                    utils.MailSend(fields['email'],sendText);
-             
-                    return;
-                });
-            });
-        });
-        res.end();
     }
-	    else
+	    else	
 	    {
-		fs.readFile(filename, function(err, data)
+		fs.readFile(filename, function(err, data) 
 		{
-			if (err)
+			if (err) 
 			{
 				console.log(filename+"NOT FOUND!\n")
 				fs.readFile('./pages/static/404/index.html',function (err, data1){
@@ -332,8 +200,8 @@ function parseCookies(request) {
 					res.end();
 				});
 				return;
-			}
-			if(filename[filename.length-1]=='/')
+			}	 
+			if(filename[filename.length-1]=='/') 
     		{
 				res.writeHead(200, {'Content-Type': 'text/html'});
 			}
@@ -341,4 +209,4 @@ function parseCookies(request) {
     		return res.end();
   		});
 	}
-}).listen(8080);
+}).listen(80);
