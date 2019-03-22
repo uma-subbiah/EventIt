f17c67ef936272d9349c55cda42c23df5284034f
Uma Subbiah <auriflore1@gmail.com>
2019-03-22 15:35:23 +0530

    Update index.js

--- Employee Portal/index.js
+++ Employee Portal/index.js
@@ -71,6 +71,51 @@ http.createServer(function (req, res)
     		return res.end();
   		});
 	}
+	else 	if(q.pathname=='/submit_m')
+	{
+		console.log("Hi");
+		var form = new formidable.IncomingForm();
+		form.parse(req,function(err,fields,files) 
+		{
+			sql.connect(config, function (err){
+				var request = new sql.Request();
+				
+				request.input('cname',fields['cname']);
+				request.input('lname',fields['lname']);
+				request.input('mobile',fields['mobile']);
+				request.input('email',fields['email']);
+				request.input('oaddress',fields['oaddress']);
+				request.input('phone',fields['phone']);
+				request.input('service',fields['service']);
+				request.input('city',fields['city']);
+				console.log('${req.body.cname}');
+				request.query("insert into media_partner(name,contact_person,mobile,email,oaddress,phone,type_,city) values(@cname,@lname,@mobile,@email,@oaddress,@phone,@service,@city);",function(err, result){
+					console.log(result);
+					console.log(fields['cname']+"\n"+err);
+					sql.close();
+					
+
+					alert('Media Partner details entered successfully')
+				});
+			});
+		});
+		var redirect = './pages/static/entered.html';
+		fs.readFile(redirect, function(err, data) 
+		{
+			if (err) 
+			{
+				console.log(filename)
+				res.writeHead(404, {'Content-Type': 'text/html'});
+				return res.end("404 Not Found!!!!!!!!");
+			}	 
+			else
+    		{
+				res.writeHead(200, {'Content-Type': 'text/html'});
+			}
+    		res.write(data);
+    		return res.end();
+  		});
+	}
 	else	
 	{
 		fs.readFile(filename, function(err, data) 
